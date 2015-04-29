bbc0ac47625841aaff52628480fead12c9031bd6
Tim Wheeler <wheelert@stanford.edu>
2015-04-29 12:03:09 -0700

    extracted more data

--- code/image_features/extract_image_features.jl
+++ code/image_features/extract_image_features.jl
@@ -3,6 +3,7 @@
 
 using Distributions
 using ImageView
+using PyPlot
 using DataFrames
 
 const INPUT_FOLDER = "/media/tim/USB DISK/Extracted/Obj_7/"
@@ -42,7 +43,7 @@ function display_image(image::Matrix{Float64}, overlay_pts::Vector{(Int,Int)};
         grayimg[i,j,:] = overlay_color
     end
 
-    view(grayimg)
+    ImageView.view(grayimg)
 end
 
 function cost_function(image::Matrix{Float64}, G::MvNormal)
@@ -215,7 +216,9 @@ function calc_xy_correlation(image::Matrix{Float64}, center_x::Int, center_y::In
     n,m = size(image)
     rad = minimum([center_x-1,center_y-1,n-center_x-1, m-center_y-1, desired_radius])
     W   = normalize_image(image)
-    @assert(rad ≥ 0)
+    if rad < 0
+        return NaN
+    end
 
     sum_xy = 0.0
     sum_x  = 0.0
@@ -247,17 +250,103 @@ function process_data(dir::String=INPUT_FOLDER;
 
     df = DataFrame(objectnumber=Array(Int, nfiles), size=Array(Int, nfiles), skew=Array(Float64, nfiles))
 
+    count = 0
     for (i,file) in enumerate(files)
-        objectnumber = int(match(r"\d+", file).match)
-        image = load_image_data(dir * file)
+        if ismatch(r"\d+", file)
+            count += 1
 
-        pts = calc_full_width_half_maximum(image)
-        mid = midpt(pts)
+            objectnumber = int(match(r"\d+", file).match)
+            image = load_image_data(dir * file)
 
-        df[i,:objectnumber] = objectnumber
-        df[i,:size] = length(pts)
-        df[i,:skew] = calc_xy_correlation(image, int(mid[1]), int(mid[2]), correlation_radius)
+            pts = calc_full_width_half_maximum(image)
+            mid = midpt(pts)
+
+            df[count,:objectnumber] = objectnumber
+            df[count,:size] = length(pts)
+            df[count,:skew] = calc_xy_correlation(image, int(mid[1]), int(mid[2]), correlation_radius)
+        end
     end
+
+    df = df[1:count, :]
 
     writetable(dir*"features.csv", df)
 end
+
+# for i in [1,2,3,4,5,6,8]
+#     dirname = "/media/tim/USB DISK/Extracted/Obj_" * string(i) * "/"
+#     println("processing ", dirname)
+#     process_data(dirname)
+# end
+
+function pull_aggregate_stats()
+
+    file_numbers = [1,2,3,4,5,6,7,8]
+
+    n_values = 0
+    for i in file_numbers
+        dirname = "/media/tim/USB DISK/Extracted/Obj_" * string(i) * "/"
+        df = readtable(dirname*"features.csv")
+        n_values += size(df, 1)
+    end
+
+    data = DataFrame(directory=Array(Int, n_values), objectnumber=Array(Int, n_values), size=Array(Int, n_values), skew=Array(Float64, n_values))
+    count = 0
+    for i in file_numbers
+        dirname = "/media/tim/USB DISK/Extracted/Obj_" * string(i) * "/"
+        df = readtable(dirname*"features.csv")
+        data[count+1:count+size(df,1),2:end] = df
+        data[count+1:count+size(df,1),:directory] = i
+        count += size(df, 1)
+    end
+
+    data
+end
+function plot_histograms(df::DataFrame)
+
+    fig = PyPlot.figure(facecolor="white")
+
+    ax1 = fig[:add_subplot](211)
+    ax1[:hist](df[:size], 100, color=[0.0, 0.5, 0.5])
+    ax1[:set_xlabel]("neuron size, FWHM [pixels]")
+    ax1[:set_ylabel]("counts")
+
+    arr = dropna(df[:skew])
+    arr = filter!(x->!isinf(x) && !isnan(x), arr)
+    ax2 = fig[:add_subplot](212)
+    ax2[:hist](arr, 100, color=[0.5, 0.0, 0.5])
+    ax2[:set_xlabel]("skew")
+    ax2[:set_ylabel]("counts")
+end
+
+function dataset_index_matching_criteria(df::DataFrame)
+
+    for i = size(df,1) : -1 : 1
+        size = df[i, :size]
+        println(size)
+        if size < 30
+            return i
+        end
+    end
+    return 0
+end
+
+function extract_dataset(df::DataFrame, i::Int)
+
+    dirnumber = df[i, :directory]
+    objnumber = df[i, :objectnumber]
+
+    dirname  = "/media/tim/USB DISK/Extracted/Obj_" * string(dirnumber) * "/"
+    filename = @sprintf("Obj_%d_1 - IC filter %d.csv", objnumber, objnumber)
+
+    image = load_image_data(dirname * filename)
+
+    pts = calc_full_width_half_maximum(image)
+
+    display_image(image, pts)
+
+    mid = midpt(pts)
+    cor = calc_xy_correlation(image, int(mid[1]), int(mid[2]), 10)
+
+    println("skew: ", cor)
+end
+
